d188e013be6701c08d061ff4861bfa1e6b4c1dae
Lennart Gastler <lennart@lgastler.de>
2022-12-11 16:31:49 +0100

    Initial Commit

---
+++ TapSudoku/TapSudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapSudokuApp.swift
+//  TapSudoku
+//
+//  Created by Lennart Gastler on 11.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TapSudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
